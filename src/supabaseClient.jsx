bbbbc0026d8f7b2aa26b403381908d3b87afd7ee
Benjamin Henke <benjaminmhenke@gmail.com>
2025-08-01 16:39:18 -0500

    supabase creds

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// TODO
-// make .env
-// bring in variables
-const supabaseUrl = 'https://crnttgodenjbjtfqqgtq.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNybnR0Z29kZW5qYmp0ZnFxZ3RxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwNzczMTAsImV4cCI6MjA2OTY1MzMxMH0.Ts6ndVPVTZLtW35hcQMKZvrHrtOmIpHy316cdxtlbGo'
+const supabaseUrl = import.meta.env.SUPABASEURL
+const supabaseKey = import.meta.env.SUPABASEKEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)

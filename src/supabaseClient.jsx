44438019b4a659d6f3487717fe06774c9db06d1a
Benjamin Henke <benjaminmhenke@gmail.com>
2025-08-01 16:44:50 -0500

    env stuff

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.SUPABASEURL
-const supabaseKey = import.meta.env.SUPABASEKEY
+const supabaseUrl = import.meta.env.SUPABASE_URL
+const supabaseKey = import.meta.env.SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
